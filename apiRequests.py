74f974b15a757e96ba40da1efbdb042318acd73f
SeaDrown <99901911+SeaDrown@users.noreply.github.com>
2025-10-25 13:45:10 +0100

    Added item functionality

--- apiRequests.py
+++ apiRequests.py
@@ -3,8 +3,9 @@ import json
 
 api_url = "https://www.guitarguitar.co.uk/hackathon"
 
-def request(endpoint):
+def request(endpoint, params):
     endpoint = endpoint or "/"
+    params = params or {}
 
-    return requests.get(api_url + endpoint)
+    return requests.get(api_url + endpoint, params)
 
--- item.py
+++ item.py
@@ -1,17 +1,28 @@
 import apiRequests
 
 endpoint = "/products"
-data = ""
 
-#try:
-thisRequest = apiRequests.request(endpoint)
+def NewItem(sku):
+    item = dict()
 
-data = thisRequest.json()
-firstItem = data[0]
+    item["SKU"] = sku
+    itemResponse = apiRequests.request(endpoint, {"SKU_ID": sku})
 
-for k, v in firstItem.items():
-    print(k +" : " + str(v))
+    if itemResponse.status_code != 200:
+        print("Request Failed")
+        return None
+    itemJson = itemResponse.json()
+    itemDict = itemJson[0]
     
-print(type(data))
-#except Exception:
-print("Failed")
+    item["Name"] = itemDict["ItemName"]
+    item["Price"] = itemDict["SalesPrice"]
+
+    return item
+
+def PrintItem(item):
+    print(item["Name"])
+    print(item["Price"])
+    print("SKU : "+ str(item["SKU"]))
+
+thisItem = NewItem(190319340844008)
+PrintItem(thisItem)
